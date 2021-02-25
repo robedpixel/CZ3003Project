d141b431e17a8b909b590f530c2b4a98cc205758
shah <h.jshah39@gmail.com>
2021-02-25 23:21:59 +0800

    Each of the SW, MC, LB scenes are independent
    The pages no longer inherit from the parent. They are individual scenes

--- Godot Project/Scripts/GUI/SceneController.gd
+++
@@ -1,5 +0,0 @@
-extends Control
-
-# a main code for the buttons in the scene pages to get back to the main menu
-func _on_Button_pressed():
-	get_tree().change_scene("res://Scenes/Main_Menu/Main Menu.tscn")
