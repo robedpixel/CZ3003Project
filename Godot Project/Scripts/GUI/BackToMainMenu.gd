327b192567a4b7662c355e87d5202d58017f4c46
shah <h.jshah39@gmail.com>
2021-03-01 13:33:15 +0800

    Added back button to the CS scene

--- Godot Project/Scripts/GUI/BackToMainMenu.gd
+++ Godot Project/Scripts/GUI/BackToMainMenu.gd
@@ -1,6 +1,7 @@
 extends Button
 
 var path_to_main_menu ="res://Scenes/Menu Scenes/Main_Menu/Main Menu.tscn";
+var path_to_select_world =  "res://Scenes/Menu Scenes/Select_World/SW.tscn";
 
 func _on_SWBackButton_pressed():
 	get_tree().change_scene(path_to_main_menu)
@@ -14,3 +15,7 @@ func _on_LBBackButton_pressed():
 
 func _on_BackButton_pressed():
 	get_tree().change_scene(path_to_main_menu)
+
+
+func _on_CSBackButton_pressed():
+	get_tree().change_scene(path_to_select_world)
