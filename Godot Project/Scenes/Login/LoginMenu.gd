d3b5d4a7678b82920672ec1c7881c75ca6754653
robedpixel <3531281-robedpixel@users.noreply.gitlab.com>
2021-03-01 08:43:26 +0800

    added indicator to login screen for failed login

--- Godot Project/Scenes/Login/LoginMenu.gd
+++ Godot Project/Scenes/Login/LoginMenu.gd
@@ -17,4 +17,5 @@ func _on_Button_pressed():
 		#get_tree().change_scene("res://Scenes/Database Test/Database Test.tscn")
 		get_tree().change_scene("res://Scenes/Menu Scenes/Main_Menu/Main Menu.tscn")
 	else:
+		$"VBoxContainer/LoginStatusLabel".set_text("Incorrect username and/or password")
 		print("cannot log in")
