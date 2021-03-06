85e1feda1e923310daa6008ea93b34b864260520
winhuiwy <winhuiwy@users.noreply.github.com>
2021-03-06 15:44:07 +0800

    added game over scene

--- Godot Project/Scenes/Database Test/Database Test.gd
+++ Godot Project/Scenes/Database Test/Database Test.gd
@@ -27,7 +27,7 @@ func _on_Button_pressed():
 	}
 	print(data)
 	#handler.save_document("leaderboard.json", data, self.http)
-	handler.save_world_score(1, 30, self.http)
+	handler.save_world_score(1, 30)
 
 
 func _on_Button2_pressed():
