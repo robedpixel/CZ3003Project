e9f9cf8e76ec101032ad02c9bb034661d64e7a81
shah <h.jshah39@gmail.com>
2021-03-21 01:20:17 +0800

    Updated MM UI

--- Godot Project/Scripts/GUI/Main Menu.gd
+++ Godot Project/Scripts/GUI/Main Menu.gd
@@ -1,20 +1,28 @@
 extends Control
 
 
-var scene_path
+var path_to_select_world = "res://Scenes/Menu Scenes/Select_World/SW.tscn"
+var path_to_maze_creator = "res://Scenes/Menu Scenes/Maze_Creator/MC.tscn"
+var path_to_leaderboard = "res://Scenes/Menu Scenes/Leaderboard/LeaderBoard.tscn"
 var x =0
 
 # for connecting the buttons to the respective buttons
 func _ready():
-	$MainMenuVericalContainer/Buttons.grab_focus() #for keyboard movements in the main menu
-	for button in $MainMenuVericalContainer/Buttons.get_children():
-		button.connect("pressed",self,"on_pressed", [button.scene_to_load])
+	pass
 
 
 # the parent scene is named as scenecontroller -> can be seen at the project folder itself
 # the children scene are at its repspective files e.g res://Select_World/SelectWorld.tscn 
 
 # on each button press, the respective page will open up
-func on_pressed(scene_to_load):
-	get_tree().change_scene(scene_to_load)
-	
+
+func _on_SelectWorldButton_pressed():
+	get_tree().change_scene(path_to_select_world)
+
+
+func _on_MazeCreatorButton_pressed():
+	get_tree().change_scene(path_to_maze_creator)
+
+
+func _on_LeaderBoardButton_pressed():
+	get_tree().change_scene(path_to_leaderboard)
