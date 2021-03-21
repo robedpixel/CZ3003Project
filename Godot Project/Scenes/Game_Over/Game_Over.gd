43b0de758eccca3339f01fe458785578d8c6adfd
winhuiwy <winhuiwy@users.noreply.github.com>
2021-03-21 17:36:36 +0800

    game over connection to analytics database

--- Godot Project/Scenes/Game_Over/Game_Over.gd
+++ Godot Project/Scenes/Game_Over/Game_Over.gd
@@ -17,7 +17,8 @@ func _saveScore(coins):
 	handler = load("res://Scripts/auth/firebase_db.gd").new()
 	add_child(handler)
 	get_node("CenterContainer/VBoxContainer/HBoxContainer/ScoreLabel").text += str(score)
-	#handler.save_world_score(world, score) #not sure how to get world and score yet
+	handler.save_world_score(topicSelected, score) #to leaderboard
+	yield(handler.save_analytics(topicSelected),"completed") #to analytics for teacher report
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://Scenes/Menu Scenes/Leaderboard/Leaderboard.tscn")
