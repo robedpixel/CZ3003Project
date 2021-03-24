0794628c1fd58b4059da09ae7783c2e6e75d80db
shah <h.jshah39@gmail.com>
2021-03-24 20:35:14 +0800

    Added back to MM for Game over scene

--- Godot Project/Scenes/Game_Over/Game_Over.gd
+++ Godot Project/Scenes/Game_Over/Game_Over.gd
@@ -1,11 +1,15 @@
 extends Node
 
+onready var leaderboard_button = get_node("CenterContainer/VBoxContainer/Button")
+
 var handler
 var score = 0
 
 func _ready():
 	handler = load("res://Scripts/auth/firebase_db.gd").new()
 	add_child(handler)
+	if(GlobalVariables.world_num==2): leaderboard_button.visible=false
+	else: leaderboard_button.visible=true
 	pass
 
 func _saveScore(coins):
@@ -28,3 +32,7 @@ func _saveScore(coins):
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://Scenes/Menu Scenes/Leaderboard/Leaderboard.tscn")
+
+
+func _on_BacktoMM_pressed():
+	get_tree().change_scene("res://Scenes/Menu Scenes/Main_Menu/Main Menu.tscn")
