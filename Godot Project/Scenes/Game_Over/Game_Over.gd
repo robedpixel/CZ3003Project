b053ce4f74cdca216faf6b0d4f1c34ada8e51d70
shah <h.jshah39@gmail.com>
2021-03-26 02:24:43 +0800

    Added social media for Game over

--- Godot Project/Scenes/Game_Over/Game_Over.gd
+++ Godot Project/Scenes/Game_Over/Game_Over.gd
@@ -1,15 +1,22 @@
 extends Node
 
 onready var leaderboard_button = get_node("CenterContainer/VBoxContainer/Button")
+onready var social_media_container = get_node("SocialMediaContainer")
 
 var handler
 var score = 0
+var student_name
 
 func _ready():
 	handler = load("res://Scripts/auth/firebase_db.gd").new()
+	print("Print world num : ",GlobalVariables.world_num)
 	add_child(handler)
-	if(GlobalVariables.world_num==2): leaderboard_button.visible=false
-	else: leaderboard_button.visible=true
+	if(GlobalVariables.world_num==2): 
+		leaderboard_button.visible=false
+		social_media_container.visible=false
+	else: 
+		leaderboard_button.visible=true
+		social_media_container.visible=true
 	pass
 
 func _saveScore(coins):
@@ -36,3 +43,19 @@ func _on_Button_pressed():
 
 func _on_BacktoMM_pressed():
 	get_tree().change_scene("res://Scenes/Menu Scenes/Main_Menu/Main Menu.tscn")
+
+
+func _on_Telegram_pressed():
+	var student_name = yield(handler.get_student_name(),"completed")
+	print(GlobalVariables.world_num)
+	GlobalVariables.post_to_telegram(GlobalVariables.world_num,GlobalVariables.score,null,student_name)
+
+
+func _on_Twitter_pressed():
+	var student_name = yield(handler.get_student_name(),"completed")
+	print(GlobalVariables.world_num)
+	GlobalVariables.post_to_twitter(GlobalVariables.world_num,GlobalVariables.score,null,student_name)
+
+
+func _on_Facebook_pressed():
+	GlobalVariables.post_to_facebook()
